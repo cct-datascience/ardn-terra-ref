4bef73030be719d204d722212caf73c4ef207775
Kristina Riemer <kristina.riemer@weecology.org>
2020-11-18 14:18:57 -0700

    Add six additional variable obs for seasons 4 and 6

--- download_observations.R
+++ download_observations.R
@@ -3,7 +3,8 @@ library(jsonlite)
 
 ###### Specify study and variable IDs ######
 studies <- c(6000000010, 6000000034)
-vars <- c(6000000196)
+vars <- c(6000000238, 6000000278, 6000000236, 6000000007, 6000000200, 
+          6000000196, 6000000193)
 
 ###### Download JSON file for each study/variable combo ######
 obs_base_url <- "https://brapi.workbench.terraref.org/brapi/v1/observationunits"
@@ -14,9 +15,11 @@ for(study in studies){
                            "&observationVariableDbId=", var, "&pageSize=2")
     pagesize_download <- fromJSON(pagesize_url)
     pagesize_count <- pagesize_download$metadata$pagination$totalCount
-    full_url <- paste0(obs_base_url, "?studyDbId=", study, 
-                           "&observationVariableDbId=", var, "&pageSize=", pagesize_count)
-    full_download <- fromJSON(full_url)
-    write(toJSON(full_download), file = paste0(paste("observations", study, var, sep = "_"), ".json"))
+    if (pagesize_count != 0) {
+      full_url <- paste0(obs_base_url, "?studyDbId=", study,
+                         "&observationVariableDbId=", var, "&pageSize=", pagesize_count)
+      full_download <- fromJSON(full_url)
+      write(toJSON(full_download), file = paste0(paste("observations", study, var, sep = "_"), ".json"))
+    }
   }
 }
