08b408cf0032a5effc3178640174d685893aff47
Kristina Riemer <kristina.riemer@weecology.org>
2022-05-31 09:48:20 -0700

    Add incomplete code to parse Brapi events JSON

--- csv_data/scripts/download_and_convert.R
+++ csv_data/scripts/download_and_convert.R
@@ -67,6 +67,83 @@ for (study in studies) {
 }
 
 ###### Table 4: Events (Fertilizer) ######
+#TODO: pull data from updated Brapi events endpoint
+
+old_events_hand_fp <- "../../TERRA_REF/brapi_terraref_repo/brapi/data/events.json"
+events_hand_fp <- "csv_data/data/events.json"
+eventParameters_cols <- c("code", "name", "description", "unit", "...1", "value", "codeValueDescription", "valuesByDate") #"value",
+events_hand <- fromJSON(events_hand_fp) %>% 
+  unnest(observationUnitDbId) %>% 
+  unnest_wider(eventParameters) %>% 
+  unnest(any_of(eventParameters_cols))
+  #unnest_wider isn't quite right
+# unnest by row and then rowbind back together? with missing columns included or something? 
+#expand observationunitdbid first and then event params/ICASA vars table? 
+
+column_names <- c("code", "unit")
+
+#can't unnest value columns due to mix of character and list
+#what to do with date column? 
+View(events_hand %>% 
+  slice(11:12) %>% 
+  unnest(observationUnitDbId) %>% 
+  mutate(eventParameters = na_if(eventParameters, "NULL")) %>% 
+  unnest_wider(eventParameters) %>% 
+  unnest(any_of(eventParameters_cols)) %>% 
+  unnest(observationUnitDbId))
+
+#if there are discrete dates, one per value if there are multiple values? unnest together?
+#unnest rows with vectors in value conditionally, both discreteDates and value
+# mutate case when row is data type of list, turn into character?
+
+
+
+# mvp would be just planting and harvest eventTypes
+
+mult_dates_ex <- events_clean_all %>% filter(eventDbId == "6000000082" | eventDbId == "6000000052")
+View(mult_dates_ex %>% unnest(date$discreteDates))
+
+# 4, 7, 9, 10, 11, 17
+events_clean_all <- c()
+for(i in 1:17){
+  print(i)
+  events_clean <- events_hand %>% 
+    slice(i) %>% 
+    unnest(observationUnitDbIds) %>% 
+    mutate(eventParameters = na_if(eventParameters, "NULL")) %>% 
+    unnest_wider(eventParameters) %>% 
+    unnest(any_of(eventParameters_cols)) %>% 
+    unpack(cols = c(date))
+  events_clean$discreteDates[sapply(events_clean$discreteDates, is.null)] <- NA
+  # number of items in discreteDates column differs from number of rows, then unnest column
+  if(length(unlist(events_clean$discreteDates)) != nrow(events_clean)){
+    events_clean <- events_clean %>%
+      unnest(discreteDates)
+  }
+  events_clean$discreteDates <- unlist(events_clean$discreteDates) #need to check if this screwed anything up
+  if("valuesByDate" %in% colnames(events_clean)){
+    events_clean <- events_clean %>% 
+      unnest(valuesByDate, keep_empty = TRUE)
+  }
+  if("value" %in% colnames(events_clean)){
+    events_clean$value <- lapply(events_clean$value, as.character)
+    
+  }
+  if("codeValueDescription" %in% colnames(events_clean)){
+    events_clean$codeValueDescription <- lapply(events_clean$codeValueDescription, as.character)
+  }
+  if("parameterDescription" %in% colnames(events_clean)){
+    events_clean$parameterDescription <- lapply(events_clean$parameterDescription, as.character)
+    
+  }
+  
+  #print(data.frame(sapply(events_clean, typeof)))
+  #print(events_clean$value)
+  events_clean_all <- bind_rows(events_clean, events_clean_all)
+}
+# events_clean_all <- events_clean_all %>% 
+#   mutate_all(~na_if(., "NULL"))
+
 events_table <- c()
 for (study in studies) {
   event_url <- paste0(base_url, "events?studyDbId=", study)
